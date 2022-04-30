1395a422a7f910e8798a2edb6e1e4fd4a9dd76c8
omegaspn <o_memaster@hotmail.com>
2022-04-30 21:53:55 +0700

    update my lotto

--- packages/vite-app-ts/src/components/pages/MyLotto.tsx
+++ packages/vite-app-ts/src/components/pages/MyLotto.tsx
@@ -1,13 +1,42 @@
-import { Row, Col } from 'antd';
-import React, { FC } from 'react';
+import { Row, Col, Modal, Typography } from 'antd';
+import React, { FC, useState } from 'react';
+
+const { Title } = Typography;
 
 import LottoCard from '../lottoCard/LottoCard';
 
 const Home: FC = () => {
+  const [lottos, setLottos] = useState([
+    {
+      reward: 500,
+      amount: 2,
+    },
+  ]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = (): void => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = (): void => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = (): void => {
+    // setIsModalVisible(false);
+  };
+
   return (
-    <div>
+    <>
+      <Modal visible={isModalVisible} onOk={handleOk} cancelButtonProps={{ style: { display: 'none' } }}>
+        <Title level={3}>Prize you will get:</Title>
+        <p>
+          {lottos[0].reward} * {lottos[0].amount} = {lottos[0].reward * lottos[0].amount} THB
+        </p>
+      </Modal>
       <Row gutter={16}>
-        <Col span={8}>
+        {/* TODO: LOOP here */}
+        <Col span={8} onClick={showModal}>
           <LottoCard lottoNumber={67} purchasedAmount={2} />
         </Col>
         <Col span={8}>
@@ -20,7 +49,7 @@ const Home: FC = () => {
           <LottoCard lottoNumber={44} purchasedAmount={6} />
         </Col>
       </Row>
-    </div>
+    </>
   );
 };
 
