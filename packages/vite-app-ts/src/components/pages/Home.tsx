fbebbece1c407877c5830220b12e47debe150bcd
omegaspn <o_memaster@hotmail.com>
2022-04-30 15:30:31 +0700

    mock home page

--- packages/vite-app-ts/src/components/pages/Home.tsx
+++ packages/vite-app-ts/src/components/pages/Home.tsx
@@ -1,7 +1,33 @@
+import { Row, Col, Statistic, Button, Image } from 'antd';
 import React, { FC } from 'react';
 
 const Home: FC = () => {
-  return <div>Homeeee pages</div>;
+  const onFinish = () => () => {
+    return null;
+  };
+
+  const { Countdown } = Statistic;
+
+  const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30; // Moment is also OK
+
+  return (
+    <div>
+      <Row>
+        <Col>Lottopus</Col>
+      </Row>
+      <Row justify="center">
+        <Countdown title="Countdown" value={deadline} onFinish={onFinish} />
+      </Row>
+      <Row justify="center">
+        <Button type="primary">Trigger Random</Button>
+      </Row>
+
+      <Image width={200} src="https://datavizproject.com/wp-content/uploads/2015/10/1-Line-Chart.png" />
+      <Row justify="center">
+        <Button type="primary">Buy</Button>
+      </Row>
+    </div>
+  );
 };
 
 export default Home;
